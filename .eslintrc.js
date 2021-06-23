aebedf59cb8ff496a88e23e6ed87251e4f756c64
96tm <khabcity@gmail.com>
2021-06-24 00:07:47 +0300

    init: setup webpack and everything else

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+    },
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# 96tm-JSFE2021Q1
-Private repository for @96tm
+# 🚀 Welcome to your new awesome project!
+
+This project has been created using **webpack-cli**, you can now run
+
+```
+npm run build
+```
+
+or
+
+```
+yarn build
+```
+
+to bundle your application
